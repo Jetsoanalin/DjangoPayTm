cdf1173dac5ff214eeed9c10a1888611dd5bb81a
jetsoanalin <jetyjetso@gmail.com>
2019-12-09 18:13:03 +0530

    checksum new file update

--- paytm/Checksum.py
+++ paytm/Checksum.py
@@ -22,6 +22,22 @@ def generate_checksum(param_dict, merchant_key, salt=None):
 
     return __encode__(hash_string, IV, merchant_key)
 
+def generate_refund_checksum(param_dict, merchant_key, salt=None):
+    for i in param_dict:    
+        if("|" in param_dict[i]):
+            param_dict = {}
+            exit()
+    params_string = __get_param_string__(param_dict)
+    salt = salt if salt else __id_generator__(4)
+    final_string = '%s|%s' % (params_string, salt)
+
+    hasher = hashlib.sha256(final_string.encode())
+    hash_string = hasher.hexdigest()
+
+    hash_string += salt
+
+    return __encode__(hash_string, IV, merchant_key)
+
 
 def generate_checksum_by_str(param_str, merchant_key, salt=None):
     params_string = param_str
@@ -67,13 +83,16 @@ def __id_generator__(size=6, chars=string.ascii_uppercase + string.digits + stri
 def __get_param_string__(params):
     params_string = []
     for key in sorted(params.keys()):
+        if("REFUND" in params[key] or "|" in params[key]):
+            respons_dict = {}
+            exit()
         value = params[key]
         params_string.append('' if value == 'null' else str(value))
     return '|'.join(params_string)
 
 
 __pad__ = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
-__unpad__ = lambda s: s[0:-(s[-1])]
+__unpad__ = lambda s: s[0:-ord(s[-1])]
 
 
 def __encode__(to_encode, iv, key):
@@ -84,7 +103,7 @@ def __encode__(to_encode, iv, key):
     to_encode = c.encrypt(to_encode)
     # Encode
     to_encode = base64.b64encode(to_encode)
-    return to_encode
+    return to_encode.decode("UTF-8")
 
 
 def __decode__(to_decode, iv, key):
@@ -93,6 +112,9 @@ def __decode__(to_decode, iv, key):
     # Decrypt
     c = AES.new(key, AES.MODE_CBC, iv)
     to_decode = c.decrypt(to_decode)
+    if type(to_decode) == bytes:
+        # convert bytes array to str.
+        to_decode = to_decode.decode()
     # remove pad
     return __unpad__(to_decode)
 
